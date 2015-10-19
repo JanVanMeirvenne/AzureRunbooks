3c4200ea9cbd2696b367b1ec6d86d45844ed7d2f
JanVanMeirvenne <jan@jvm-net.com>
2015-10-19 22:40:28 +0200

    Updated Set-LocalDNSRecord.ps1 in Azure Automation.

--- Set-LocalDNSRecord.ps1
+++ Set-LocalDNSRecord.ps1
@@ -1,22 +1,20 @@
 param(
-		
-		
 		[string] $Record,
 		[string] $Zone,
 		[string] $Type = 'A',
 		[string] $IP
 	)
 
-$Credential = Get-AutomationPSCredential -Name 'On-prem Admin'
-$ComputerName = Get-AutomationVariable -Name 'DNS-Server'
+$Cred = Get-AutomationPSCredential -Name 'On-prem Admin'
+$Computer = Get-AutomationVariable -Name 'DNS-Server'
 
-invoke-command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
-	param(
+invoke-command -ComputerName $Computer -Credential $Cred -ScriptBlock {
+	
 		$Record = $args[0],
 		$Zone = $args[1],
 		$Type = $args[2],
 		$IP = $args[3]
-	)
+	
 	import-module dnsserver
 	$NewObj = Get-DnsServerResourceRecord -Name $Record -ZoneName $Zone -RRType $Type
 	$OldObj = Get-DnsServerResourceRecord -Name $Record -ZoneName $Zone -RRType $Type
@@ -24,5 +22,3 @@ invoke-command -ComputerName $ComputerName -Credential $Credential -ScriptBlock 
 	Set-DnsServerResourceRecord -NewInputObject $NewObj -OldInputObject $OldObj -ZoneName $Zone
 	
 } -ArgumentList $Record,$Zone,$Type,$IP
-
-	
