aca5832ba8b92b4644e5297b4756b13cf4b1a408
JanVanMeirvenne <jan@jvm-net.com>
2015-10-20 13:10:24 +0200

    Updated Connect-Azure.ps1 in Azure Automation.

--- Connect-Azure.ps1
+++ Connect-Azure.ps1
@@ -1,5 +1,7 @@
 workflow Connect-Azure
 {
 	Add-AzureAccount -Credential (Get-AutomationPSCredential -name 'Azure')
+	Set-AzureSubscription -SubscriptionName "Pay-As-You-Go" ` -CurrentStorageAccountName (Get-AzureStorageAccount).Label -PassThru
+
 	
 }
