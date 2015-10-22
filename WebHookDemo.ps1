34de9240ffdf48a94739f6f4336273f5ad8d4918
JanVanMeirvenne <jan@jvm-net.com>
2015-10-22 13:04:04 +0200

    Updated WebHookDemo.ps1 in Azure Automation.

--- WebHookDemo.ps1
+++ WebHookDemo.ps1
@@ -8,10 +8,27 @@ workflow WebHookDemo
 	 $WebhookName    =   $WebhookData.WebhookName
      $WebhookHeaders =   $WebhookData.RequestHeader
      $WebhookBody    =   $WebhookData.RequestBody
-	 
+	 $IP = ""
 	 $Data = ConvertFrom-Json -InputObject $WebhookBody
+	 if($Data.IP -eq $null){
+		 Connect-Azure
+		 $Job = Start-AzureAutomationRunbook –AutomationAccountName "jvm-net-sma" –Name "Get-PublicIP" -RunOn 'Azure01'
+	     $doLoop = $true
+		While ($doLoop) {
+   			$job = Get-AzureAutomationJob –AutomationAccountName "jvm-net-sma" -Id $job.Id
+   			$status = $job.Status
+   			$doLoop = (($status -ne "Completed") -and ($status -ne "Failed") -and ($status -ne "Suspended") -and ($status -ne "Stopped"))
+		}
+		
+		$Output = Get-AzureAutomationJobOutput –AutomationAccountName "jvm-net-sma" -Id $job.Id –Stream Output
+		$IP = (string) $Output
+		} else {
+			$IP = $Data.IP	
+		}
+
+
 	 
-	 $Output = Set-NamecheapDNSRecord -Record $Data.Record
+	 $Output = Set-NamecheapDNSRecord -Record $Data.Record -IP $IP
 	 write-output "Data received from $($WebhookHeaders.From): Input: Record = $($Data.Record)|Output: $($Output)"
 	 
 }
