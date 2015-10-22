60b0c08bfa82443c9766caee2966347f811f022e
JanVanMeirvenne <jan@jvm-net.com>
2015-10-22 11:09:04 +0200

    Updated Set-GlobalDNSRecord.ps1 in Azure Automation.

--- Set-GlobalDNSRecord.ps1
+++ Set-GlobalDNSRecord.ps1
@@ -2,13 +2,14 @@ workflow Set-GlobalDNSRecord
 {
 	param(
 		[String] $Record,
-		[String] $WANIP,
+		[String] $WANIP = "",
 		[String] $LANIP = "127.0.0.1"
 	)
 	
-	if($WANIP -eq $null){
+	if($WANIP -eq ""){
 		write-verbose 'WAN parameter empty, obtaining public ip for this machine'
-		$WANIP = Get-PublicIP
+		$Output = Get-PublicIP
+		$WANIP = $Output
 		write-debug "WAN IP Detected: $WANIP"
 	}
 	
